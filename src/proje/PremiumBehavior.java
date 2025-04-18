4ef3a8bf9cddb420e0e9b815622944c11bcce40e
Amiro <ramezanpoor218@gmail.com>
2025-04-18 12:40:49 +0330

    creat bace class

---
+++ src/proje/PremiumBehavior.java
@@ -0,0 +1,21 @@
+package proje;
+
+public class PremiumBehavior implements UserBehavior{
+    int month;
+    
+    @Override
+    public void createPlaylist(String title, User owner) {
+        Playlist playlist = new Playlist (title, owner);
+        owner.playlists.add(playlist);
+    }
+
+    @Override
+    public void playMusic(Music music) {
+        music.play();
+    }
+
+    @Override
+    public void buyPremium(User owner, int month) {
+        this.month += month;
+    }
+}
